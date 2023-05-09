b5fc4a879e5e82164f08434c241a1796d20c3e36
Ivan <black.rook96@gmail.com>
2023-05-09 19:19:58 +0300

    Убрал все лишнее и переименовал проекты

--- Computer_vision/project.py
+++ Computer_vision/project.py
@@ -3,32 +3,43 @@ from tensorflow.keras.applications.resnet import ResNet50
 from tensorflow.keras.layers import GlobalAveragePooling2D, Dense
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.optimizers import Adam
-from sklearn.metrics import  mean_absolute_error as MAE
+from tensorflow.keras.losses import MeanAbsoluteError, Huber
+import pandas as pd
 
 
 def load_train(path):
-    datagen = ImageDataGenerator(validation_split=0.25, 
+    df = pd.read_csv(f'{path}/labels.csv')
+    datagen = ImageDataGenerator(validation_split=0.25,
                                 rescale=1/255,
                                 rotation_range = 90,
                                 horizontal_flip=True,
                                 width_shift_range= .2,
                                 height_shift_range = .2)
-    train_datagen_flow = datagen.flow_from_directory(path,
+    train_datagen_flow = datagen.flow_from_dataframe(directory = f'{path}/final_files',
+                                                    dataframe=df,
                                                     target_size=(244, 244),
                                                     batch_size=16,
-                                                    class_mode='sparse',
+                                                    class_mode='raw',
                                                     subset='training',
+                                                    x_col = 'file_name',
+                                                    y_col = 'real_age',
                                                     seed=984651)
+
     
 
     return train_datagen_flow
 
 def load_test(path):
-    test_datagen = ImageDataGenerator(rescale=1/255)
-    test_datagen_flow = test_datagen.flow_from_directory(path,
+    df = pd.read_csv(f'{path}/labels.csv')
+    test_datagen = ImageDataGenerator(validation_split=0.25,rescale=1/255)
+    test_datagen_flow = test_datagen.flow_from_dataframe(directory = f'{path}/final_files',
+                                                         dataframe=df,
                                                          target_size=(244,244),
                                                          batch_size=16,
-                                                         class_mode='sparse',
+                                                         class_mode='raw',
+                                                         subset='validation',
+                                                         x_col = 'file_name',
+                                                         y_col = 'real_age',
                                                          seed=984651)
 
     return test_datagen_flow
@@ -37,25 +48,25 @@ def load_test(path):
 
 def create_model(input_shape):
     backbone = ResNet50(input_shape=input_shape,
-                    weights='/datasets/keras_models/resnet50_weights_tf_dim_ordering_tf_kernels_notop.h5',
+                    weights='imagenet',
                     include_top=False) 
-    optimizer = Adam(Ir=.0001)
+    optimizer = Adam(lr=0.00001)
+    loss = Huber()
     model = Sequential()
     model.add(backbone)
     model.add(GlobalAveragePooling2D())
-    model.add(Dense(12, activation='softmax')) 
-    model.compile(optimizer=optimizer, loss='sparse_categorical_crossentropy',
-                  metrics=['mae'])
-
+    model.add(Dense(50, activation='relu')) 
+    model.add(Dense(1, activation='relu')) 
+    model.compile(optimizer=optimizer, loss=loss,metrics=['mae'])
     return model
 
 
-def train_model(model, train_data, test_data, batch_size=None, epochs=10,
+def train_model(model, train_data, test_data, batch_size=None, epochs=20,
                 steps_per_epoch=None, validation_steps=None):
     model.fit(train_data,
+              validation_data=test_data,
               batch_size=batch_size, epochs=epochs,
               steps_per_epoch=steps_per_epoch,
               validation_steps=validation_steps,
               verbose=2)
-    
-    return print(model.predict(test_data))
+    return model
