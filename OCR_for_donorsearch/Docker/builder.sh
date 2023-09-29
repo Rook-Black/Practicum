d4bcafd652703e4bffd91fd219a9066b64eeaff5
BlackRook <black.rook96@gmail.com>
2023-09-29 11:17:13 +0300

    docker

--- OCR_for_donorsearch/Docker/builder.sh
+++ OCR_for_donorsearch/Docker/builder.sh
@@ -1,4 +1,4 @@
-docker build -t docker push rookblack/ocr_donor:redis -f Dockerfile.redis ../
-docker build -t docker push rookblack/ocr_donor:web -f Dockerfile.web ../
-docker build -t docker push rookblack/ocr_donor:worker -f Dockerfile.worker ../
+docker build -t rookblack/ocr_donor:redis -f Dockerfile.redis ../
+docker build -t rookblack/ocr_donor:web -f Dockerfile.web ../
+docker build -t rookblack/ocr_donor:worker -f Dockerfile.worker ../
 docker compose up
